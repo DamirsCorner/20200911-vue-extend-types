76f345e6b1ea5a6f993c69252cffb792f671ccea
Damir Arh <damir.arh@gmail.com>
2020-08-24 15:33:12 +0200

    Vue Router example

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div><a @click="goBack()">Back</a></div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class extends Vue {
+  goBack() {
+    window.history.length > 0 ? this.$router.go(-1) : this.$router.push("/");
+  }
+}
+</script>
